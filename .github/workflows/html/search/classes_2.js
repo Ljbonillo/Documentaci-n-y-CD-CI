1c25663cdb51997141ed781f8091a3204a4242f9
Ljbonillo <86267865+Ljbonillo@users.noreply.github.com>
2025-05-05 14:08:06 +0200

    Delete .github/workflows/html directory

--- .github/workflows/html/search/classes_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['repositorio_0',['Repositorio',['../interfaceorg_1_1example_1_1_repositorio.html',1,'org::example']]]
-];
--- .github/workflows/html/search/functions_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['calculadoraservice_0',['CalculadoraService',['../classorg_1_1example_1_1_calculadora_service.html#ae66de4273a4fb3578c7a06aa6073f36f',1,'org::example::CalculadoraService']]]
-];
